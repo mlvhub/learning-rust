a5e8e5e8454fe9fa48cb4570b04cb16d7ca2e30d
Miguel Lopez V <miguellova@gmail.com>
2018-07-21 11:58:16 +0100

    Async book server

--- book-server/src/bin/main.rs
+++ book-server/src/bin/main.rs
@@ -3,16 +3,22 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs::File;
 
+extern crate bookserver;
+use bookserver::ThreadPool;
+
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
